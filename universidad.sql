5a2d540a33aac6b60591a89110e2d5883832bc75
Ale.G0mez15 <alej3839@gmail.com>
2026-04-09 12:17:34 +0100

    Hecho
    Comprobación y trabajo hecho, faltaterminar la presentación.

--- universidad.sql
+++ universidad.sql
@@ -1,7 +1,6 @@
--- SCRIPT EN REVISIÓN----
-
 -- Crear Base de Datos "universidad" ----
 CREATE DATABASE universidad;
+
 -- Usar la Base de Datos "universidad" ----
 USE universidad;
 -- Crear las tablas----
@@ -40,7 +39,7 @@ CREATE TABLE alumnos (
   codigoPostal INT UNSIGNED NOT NULL,
   municipio VARCHAR(55) NOT NULL,
   provincia VARCHAR(55) NOT NULL,
-  categoria VARCHAR(50) NOT NULL
+  beca VARCHAR(4) NOT NULL
 );
 
 -- asignaturas ---
@@ -82,12 +81,17 @@ CREATE TABLE tlfContactoProf (
 
 -- Importar todos los datos de cada tabla ----
 
+-- Problema resuelto: La gran mayoria de ficheros Csv tenian saltos de linea de Windows (\r), --
+-- es probable que fuera uno de los princpales problemas al insertar los FK y el error 1452, y ---
+-- además que las consultas salgan mal. Para ello, a la hora de importar cada tabla, por si acaso, --
+-- se le pone que termine de importar filas si hay un "\r\n" o "\r" o "\n". --
+
 -- Alumnos ----
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_profe/alumnos.csv'
 INTO TABLE alumnos
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 -- Cursos ----
@@ -95,7 +99,7 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_pr
 INTO TABLE cursos
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 -- Profesores ----
@@ -103,29 +107,29 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_pr
 INTO TABLE profesores
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 -- Asignaturas ----
-
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_profe/asignatura.csv'
 INTO TABLE asignaturas
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n' -- Problema resuelto: Csv tenia saltos de linea de Windows (\r\n), es probable que fuera uno de los princpales problemas al insertar los FK y el error 1452, y además, al hacer las consultas, el coordinador PR005 era el único que salía, casualmente, no tenía ese salto de linea raro.--
+LINES TERMINATED BY '\r\n' 
 IGNORE 1 ROWS;
 
--- Matricula ----
+-- Matricula (Cuidado, no tiene PK, se puede importar tantas veces como se quiera)----
+ALTER TABLE matricula ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY; -- Solucion: Agregar un id unico a cada fila, para no repetir (ANTES DE EJECUTAR ESTA LINEA, PRIMERO HAY QUE IMPORTAR UNA VEZ LA TABLA.---
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_profe/matricula.csv'
 INTO TABLE matricula
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
--- Impartir (Cuidado, no tiene PK, se puede importar tantas veces como se quiera)----
-ALTER TABLE impartir ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY; -- Solucion: Agregar un id unico a cada fila, para no repetir.---
+-- Impartir (Mismo error que la tabla "matricula")----
+ALTER TABLE impartir ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_profe/impartir.csv'
 INTO TABLE impartir
 FIELDS TERMINATED BY ','
@@ -138,11 +142,10 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/carpeta_de_la_pr
 INTO TABLE tlfContactoProf
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 -- Consultas ----
-
 
 -- 1. Información de cada asignatura con estadísticas de notas (Correcto)
 SELECT a.idCurso AS curso, a.nombre, a.caracter, COUNT(m.idAlumno) AS alumnos, MIN(m.nota) AS nota_min, MAX(m.nota) AS nota_max, AVG(m.nota) AS nota_media
@@ -187,11 +190,11 @@ ON p.idProfesor = i.idProfesor
 GROUP BY p.idProfesor
 ORDER BY asignaturas DESC;
 
--- 6. Alumnos con media mayor que 7
+-- 6. Alumnos con media mayor que 7 (Correcto)
 SELECT *
 FROM (
 SELECT a.nombre, a.apellido1, a.apellido2, AVG(m.nota) AS media
-FROM alumno a
+FROM alumnos a
 JOIN matricula m
 ON a.idAlumno = m.idAlumno
 GROUP BY a.idAlumno
@@ -199,67 +202,67 @@ GROUP BY a.idAlumno
 WHERE media > 7
 ORDER BY media DESC;
 
--- 7. Créditos totales por curso y carácter
-SELECT curso, caracter, SUM(creditos) AS total_creditos
-FROM asignatura
-GROUP BY curso, caracter;
+-- 7. Créditos totales por curso y carácter (Correcto)
+SELECT idCurso, caracter, SUM(creditos) AS total_creditos
+FROM asignaturas
+GROUP BY idCurso, caracter;
 
--- 8. Asignaturas optativas sin alumnos
-SELECT a.idAsignatura, a.nombre, a.curso
-FROM asignatura a
+-- 8. Asignaturas optativas sin alumnos (Correcto)
+SELECT a.idAsignatura, a.nombre, a.idCurso
+FROM asignaturas a
 LEFT JOIN matricula m
 ON a.idAsignatura = m.idAsignatura
 WHERE a.caracter = 'Optativa'
 AND m.idAlumno IS NULL;
 
--- 9. Alumnos que van a recuperación en asignaturas de primero
+-- 9. Alumnos que van a recuperación en asignaturas de primero (Correcto) 
 SELECT a.nombre, COUNT(m.idAlumno) AS alumnos_recuperacion
-FROM asignatura a
+FROM asignaturas a
 JOIN matricula m
 ON a.idAsignatura = m.idAsignatura
-WHERE a.curso = 1
+WHERE a.idCurso = 1
 AND m.nota < 5
 GROUP by a.idAsignatura;
 
--- 10. Alumnos que suspendieron Álgebra lineal
+-- 10. Alumnos que suspendieron Álgebra lineal (Correcto)
 SELECT al.nombre, al.apellido1, al.apellido2, m.nota
-FROM alumno al
+FROM alumnos al
 JOIN matricula m
 ON al.idAlumno = m.idAlumno
-JOIN asignatura a
+JOIN asignaturas a
 ON m.idAsignatura = a.idAsignatura
 WHERE a.nombre = 'Álgebra lineal'
 AND m.nota < 5;
 
--- 11. Alumnos de segundo curso con nota 10
+-- 11. Alumnos de segundo curso con nota 10 (Correcto)
 SELECT a.nombre, a.apellido1, a.apellido2, asig.nombre, m.nota
-FROM alumno a
+FROM alumnos a
 JOIN matricula m
 ON a.idAlumno = m.idAlumno
-JOIN asignatura asig
+JOIN asignaturas asig
 ON m.idAsignatura = asig.idAsignatura
-WHERE asig.curso = 2
+WHERE asig.idCurso = 2
 AND m.nota = 10
 ORDER BY asig.nombre;
 
--- 12. Número total de alumnos y alumnos con beca
-SELECT COUNT(*) AS total_alumnos, SUM(beca = 'Sí') AS alumnos_beca
-FROM alumno;
+-- 12. Número total de alumnos y alumnos con beca (Correcto)
+SELECT COUNT(*) AS total_alumnos, SUM(beca = 'si') AS alumnos_beca, ROUND(SUM(beca = 'si') * 100.0 / COUNT(*), 2) AS porcentaje_becados
+FROM alumnos;
 
--- 13. Nota media de alumnos becados
-SELECT a.nombre, a.apellido1, a.apellido2, asig.curso, AVG(m.nota) AS media
-FROM alumno a
+-- 13. Nota media de alumnos becados (Correcto)
+SELECT a.nombre, a.apellido1, a.apellido2, asig.idCurso, AVG(m.nota) AS media
+FROM alumnos a
 JOIN matricula m
 ON a.idAlumno = m.idAlumno
-JOIN asignatura asig
+JOIN asignaturas asig
 ON m.idAsignatura = asig.idAsignatura
-WHERE a.beca = 'Sí'
-GROUP BY a.idAlumno, asig.curso
-ORDER BY asig.curso;
+WHERE a.beca = 'si'
+GROUP BY a.idAlumno, asig.idCurso
+ORDER BY a.nombre, a.apellido1, a.apellido2, asig.idCurso;
 
--- 14. Profesores con mejor media
+-- 14. Profesores con mejor media (Correcto)
 SELECT p.nombre, p.apellido1, p.apellido2, AVG(m.nota) AS media
-FROM profesor p
+FROM profesores p
 JOIN impartir i
 ON p.idProfesor = i.idProfesor
 JOIN matricula m
@@ -268,16 +271,16 @@ GROUP BY p.idProfesor
 ORDER BY media DESC
 LIMIT 10;
 
--- 15. Asignaturas con
+-- 15. Asignaturas con %datos% y %progra% (Correcto)
 SELECT *
-FROM asignatura
+FROM asignaturas
 WHERE nombre LIKE '%datos%'
 OR nombre LIKE '%progra%'
-ORDER BY curso, nombre;
+ORDER BY idCurso, nombre;
 
--- 16. Lista de personas
+-- 16. Lista de personas (Correcto)
 SELECT nombre, apellido1, apellido2, email
-FROM alumno;
+FROM alumnos;
 
 SELECT nombre, apellido1, apellido2, email
-FROM profesor;
+FROM profesores;
